8a5709ad3842ab840ce85dac7e3701aa024b67d3
Jesterhearts <drogers@drogerspc.pc.factset.com>
2015-09-04 14:14:46 -0700

    Adding private member access
    Broken in VS2015RC

--- reflect.h
+++ reflect.h
@@ -5,6 +5,9 @@
 #include "private/member_typemap.h"
 
 namespace reflect {
+#define REFLECT_PRIVATE_MEMBERS()   \
+    template<typename> friend struct reflect::detail::class_reflection_info;
+
 template<typename Class>
 auto reflect(Class& instance) {
     return detail::class_reflection_info<Class>::reflect(instance);
