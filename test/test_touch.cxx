7874b9c2e717864fbd1f3de41a4ae41e50b9fcbe
Jesterhearts <j.tl.summers@gmail.com>
2017-04-18 19:50:31 -0700

    rename access to touch

--- test/test_touch.cxx
+++ test/test_touch.cxx
@@ -1,97 +1,95 @@
 #include "testclasses.h"
 
-#include <memory>
-
-TEST_CASE("basic member access", "[reflection][access][basic]") {
+TEST_CASE("basic member touch", "[reflection][touch][basic]") {
    Foo f{};
    auto reflected = reflect::reflect(f);
 
-   SECTION("member pod variable access") {
+   SECTION("member pod variable touch") {
       REQUIRE_NOTHROW(reflected["i"].get_type());
    }
 
-   SECTION("member object variable access") {
+   SECTION("member object variable touch") {
       REQUIRE_NOTHROW(reflected["string"].get_type());
    }
 
-   SECTION("member function access") {
+   SECTION("member function touch") {
       REQUIRE_NOTHROW(reflected["Bar"].get_type());
    }
 
-   SECTION("non-existent member access") {
+   SECTION("non-existent member touch") {
       REQUIRE_THROWS_AS(reflected["invalid identifier"], const reflect::member_access_error&);
    }
 
 }
 
-TEST_CASE("static member access", "[reflection][access][static]") {
+TEST_CASE("static member touch", "[reflection][touch][static]") {
    Static s;
    auto reflected = reflect::reflect(s);
 
-   SECTION("static variable access") {
+   SECTION("static variable touch") {
       REQUIRE_NOTHROW(reflected["abc"].get_type());
    }
 
-   SECTION("static function access") {
+   SECTION("static function touch") {
       REQUIRE_NOTHROW(reflected["Funky"].get_type());
    }
 
 }
 
-TEST_CASE("const member access", "[reflection][access][const]") {
+TEST_CASE("const member touch", "[reflection][touch][const]") {
    Const const_;
    auto reflected = reflect::reflect(const_);
 
-   SECTION("const member variable access") {
+   SECTION("const member variable touch") {
       REQUIRE_NOTHROW(reflected["_const"].get_type());
    }
 
-   SECTION("static const member variable access") {
+   SECTION("static const member variable touch") {
       REQUIRE_NOTHROW(reflected["_static"].get_type());
    }
 
 }
 
-TEST_CASE("array and pointer member access", "[reflection][access][arrays]") {
+TEST_CASE("array and pointer member touch", "[reflection][touch][arrays]") {
    Arrays arrays;
    auto reflected = reflect::reflect(arrays);
 
-   SECTION("pointer member access") {
+   SECTION("pointer member touch") {
       REQUIRE_NOTHROW(reflected["iptr"].get_type());
    }
 
-   SECTION("dynamic array member access") {
+   SECTION("dynamic array member touch") {
       REQUIRE_NOTHROW(reflected["iarr"].get_type());
    }
 
-   SECTION("sized array member access") {
+   SECTION("sized array member touch") {
       REQUIRE_NOTHROW(reflected["iarr10"].get_type());
    }
 
 }
 
-TEST_CASE("empty object member access", "[reflection][access][empty]") {
-   SECTION("non static member access") {
+TEST_CASE("empty object member touch", "[reflection][touch][empty]") {
+   SECTION("non static member touch") {
       auto reflected = reflect::reflect<Foo>();
       REQUIRE_NOTHROW(reflected["i"].get_type());
    }
 
-   SECTION("static const member access") {
+   SECTION("static const member touch") {
       auto reflected = reflect::reflect<Const>();
       REQUIRE_NOTHROW(reflected["_static"].get_type());
    }
 
-   SECTION("static member access") {
+   SECTION("static member touch") {
       auto reflected = reflect::reflect<Static>();
       REQUIRE_NOTHROW(reflected["abc"].get_type());
    }
 
-   SECTION("static function access") {
+   SECTION("static function touch") {
       auto reflected = reflect::reflect<Static>();
       REQUIRE_NOTHROW(reflected["Funky"].get_type());
    }
 
-   SECTION("non-existent member access") {
+   SECTION("non-existent member touch") {
       auto reflected = reflect::reflect<Static>();
       REQUIRE_THROWS_AS(reflected["invalid identifier"], const reflect::member_access_error&);
    }
